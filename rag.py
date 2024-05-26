3d57d36197b5076404f3c74294dd78439536523d
M Kim <minskim010203@gmail.com>
2024-05-26 12:00:16 +0000

    rag.py with prompt

--- rag.py
+++ rag.py
@@ -11,60 +11,70 @@ from langchain_core.output_parsers import StrOutputParser
 import faiss
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
 import os
-
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-os.environ['HF_HOME'] = "/home/jovyan/team_3/"
+from utils.arguments import parse_arguments
 
 
-vectordb = FAISS.load_local("./", HuggingFaceBgeEmbeddings(), allow_dangerous_deserialization=True)
+if __name__=="__main__":
+    
+    args = parse_arguments()
+    
+    if not args.prompt:
+        args.prompt = input("어떤 화장품을 추천받고 싶으신가요?:\n")
+    
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
-RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
-retriever = vectordb.as_retriever(
-    search_kwargs={"k": 5} # top-10
-)
 
-compression_retriever = ContextualCompressionRetriever(
-    base_compressor=RAG.as_langchain_document_compressor(), base_retriever=retriever
-)
+    vectordb = FAISS.load_local(args.index_dir, HuggingFaceBgeEmbeddings(), allow_dangerous_deserialization=True)
 
-### TODO: create prompt! ###
+    RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
+    
+    retriever = vectordb.as_retriever(
+        search_kwargs={"k": 5} # top-10
+    )
 
-# prompt = '' # format prompt...
-
-prompt = ChatPromptTemplate(
-    messages=[HumanMessagePromptTemplate(prompt=PromptTemplate(
-        input_variables=["context", "question"],
-        template="### 리뷰와 화장품 정보를 참고해서 사용자에게 화장품을 추천해주고 그 이유도 알려줘. \n\n ### 사용자: {question}\n\n### 리뷰: {context}\n\n### 추천: "
-        ))],
-    input_variables=["context", "question"]
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=RAG.as_langchain_document_compressor(), base_retriever=retriever
     )
 
 
-    
+    prompt = ChatPromptTemplate(
+        messages=[HumanMessagePromptTemplate(prompt=PromptTemplate(
+            input_variables=["context", "question"],
+            template="### 과제: 너는 화장품 추천 챗봇이야. 리뷰와 화장품 정보를 참고해서 사용자에게 맞는 화장품을 친절하게 추천해주고 그 이유도 알려줘.\n\n### 사용자: {question}\n\n### 리뷰: {context}\n\n### 추천: \n상품명: \n가이드: "
+            ))],
+        input_variables=["context", "question"]
+        )
 
-### TODO: load hf model! ###
-#repo_id='yanolja/EEVE-Korean-10.8B-v1.0'
-repo_id='mistralai/Mistral-7B-Instruct-v0.2'
-llm = HuggingFaceEndpoint(
-    repo_id=repo_id, temperature=0.1, max_length=512
-)
+    llm = HuggingFaceEndpoint(
+        repo_id=args.model_id, temperature=0.1, model_kwargs={"max_length":2048}
+    )
 
-# merge retrieved document
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+    # merge retrieved document
+    def format_docs(docs):
+        new_docs = []
+        
+        for doc in docs:
+            metadata = doc.metadata
+            review = doc.page_content
+            product = metadata["product"]
+            product_str = "##상품:{name}\n\t-브랜드: {brand}\n\t-가격: {price}\n\t-리뷰 수: {total_reviews}\n\t-별점 평균: {average_star_rating}\n".format_map(product)
+            review_str= f"##리뷰: {review}\n\t-리뷰 날짜: {metadata['date']}\n\t-리뷰 별점: {metadata['score']}\n\t-태그: {metadata['tags']}\n"
+            
+            new_docs.append("".join([product_str, review_str]))
+        
+        return "---\n\n".join(new_doc for new_doc in new_docs)
 
 
-# 단계 8: 체인 생성(Create Chain)
-rag_chain = (
-    {"context": compression_retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+    # 단계 8: 체인 생성(Create Chain)
+    rag_chain = (
+        {"context": compression_retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
 
 
-question = "민감성 피부를 위한 제품 추천해죠"
-response = rag_chain.invoke(question)
-# response = rag_chain.invoke(question)
+    # question = "민감성 피부를 위한 제품 추천해죠"
+    response = rag_chain.invoke(args.prompt)
 
-print(response)
+    print(response)
