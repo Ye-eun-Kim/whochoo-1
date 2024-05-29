64a2f0f1e4220e9b67f49dadd2e70fab19f6ff30
M Kim <minskim010203@gmail.com>
2024-05-29 01:09:45 +0000

    rag+streamlit

--- utils/arguments.py
+++ utils/arguments.py
@@ -7,8 +7,10 @@ def parse_arguments():
     parser.add_argument("--metadata_path", type=str, default="./metadata.json")
     parser.add_argument("--data_type", type=str, default="json", choices=["json", "csv"])
     parser.add_argument("--data_path", type=str, default="./data.json")
-    parser.add_argument("--model_id", type=str, default="mistralai/Mistral-7B-Instruct-v0.2")
+    parser.add_argument("--model_id", type=str, default="meta-llama/Meta-Llama-3-8B-Instruct")
     parser.add_argument("--prompt", type=str, default=None)
+    parser.add_argument("--token", type=str, default="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", help="Please provide valid HF token.") # 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
+    parser.add_argument("--top_k", type=int, default=10)
     
     args = parser.parse_args()
     
